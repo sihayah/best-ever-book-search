6b1039380c25dadddc7af3fdbaef90cf9df4b1e6
Sihayah <sihaya_a@hotmail.com>
2022-03-08 16:41:42 -0500

    added apollo-server to package-json, updated server.js to use apollo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+// const {authMiddleware} = require(....)
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authmiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app })
+  console.log(`Use GraphQL at http://locahost:${PORT}${server.graphqlPath}`);
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +28,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
